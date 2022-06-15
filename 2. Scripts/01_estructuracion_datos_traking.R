e1c8c0b5e1db82eacc16cb84c71b53ae615ddf0a
Guibor Camargo <guibor.camargo@urosario.edu.co>
2022-06-14 21:37:28 -0500

    Avances

--- 2. Scripts/01_estructuracion_datos_traking.R
+++ 2. Scripts/01_estructuracion_datos_traking.R
@@ -1,10 +1,11 @@
 # Revisión de datos de movilidad
 
 
-# 0. Pasos previos
+# 0. Pasos previos ----
 
 # 0.1. Librerias
 library(dplyr)
+library(ggplot2)
 
 # 0.2. Datos
 a0_raw <- readRDS("0. Datos/1. Serivi-informacion 2020.RDS")
@@ -12,8 +13,26 @@ a0_raw <- readRDS("0. Datos/1. Serivi-informacion 2020.RDS")
 names(a0_raw)
 paste0("From ",min(a0_raw$date)," to ",max(a0_raw$date))
 
-# 2.
+# 2. Resumenes iniciales ----
 
+## 2.1. Traking points por ID y mes  ----
+R1_IDs_por_mes <- a0_raw %>% 
+  # Determinar mes del punto
+  mutate(month = as.Date(paste0(substr(date,1,8),"01"))) %>% 
+  # Agrupar por ID y mes
+  group_by(identifier, month) %>% 
+  summarize(poitns = n()) %>% 
+  as.data.frame()
+  
+## 2.2. Identificación de ID's sobrevivientes ----
+  ###
+R2_IDs_sobrevivientes <- R1_IDs_por_mes %>% 
+  group_by(identifier) %>% 
+  mutate(peridos = n()) %>% 
+  as.data.frame()
+  
+pr %>% ggplot(aes(peridos)) +
+  geom_bar()
 
 
 # View(head(a0_raw,100))
--- README.md
+++ README.md
@@ -3,6 +3,18 @@ This repository contains the data and the scripts used in the articule "Urban qu
 
 
 ## Trakign data info
+### Pre-filtered data
 - 123'791.061 of traking points
 - From 2020-01-01 to 2020-12-31 (365 days)
 - 524.020 different IDs
+
+
+### Post-filtered data
+The sample contians exactly 12 months of traking data, however not all the users have a permant record over each month, so we aplied 3 rules to extract a final sample:
+
+1. Individuals must have data over at least 10 months
+2. Individuals must have at leas 60 traking poitns per month
+3. Individuals must live inside Bogota's urban limit during the analized period.
+
+Taking 
+
